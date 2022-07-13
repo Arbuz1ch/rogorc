54dc4d500c4dc251e567da16cba583b57e8d8421
Berty <winestnik@gmail.com>
2022-07-14 00:57:34 +0400

    Мелкие фиксы главной страницы, переработка функционала формы

--- js/form.js
+++ js/form.js
@@ -1,5 +1,48 @@
  const TOKEN = '5177947153:AAFSWGOf6Zc_K009UwV28R4t9kJVz-POKZA';
  const CHAT_ID = '-635333222';
+ 
+ const element = document.getElementById('labelform');
+
+ window.onload = () => {
+	const select = document.getElementById('select');
+
+	select.addEventListener('change', (event) => {
+		event.preventDefault();
+
+		let status = select.value;
+
+        if (status == 'Доставка') {
+            createRecieveElement();
+        } else {
+            hideRecieveElement();
+        }
+
+	});
+ };
+
+ function createRecieveElement() {
+
+    const divElement = document.createElement('div');
+    divElement.classList.add('dosDiv');
+
+    let label = document.createElement('label');
+    label.textContent = 'Введите адрес доставки';
+
+    const inputText = document.createElement('input');
+    inputText.setAttribute('type', 'text');
+    inputText.setAttribute('placeholder', 'Введите адрес доставки');
+    inputText.setAttribute('name', 'adress');
+    inputText.classList.add('inputText');
+
+    divElement.appendChild(label);
+    divElement.appendChild(inputText);
+    element.appendChild(divElement);
+ }
+
+ function hideRecieveElement() {
+	let label = document.querySelector('.dosDiv');
+    element.removeChild(label);
+ }
 
  document.getElementById('button').addEventListener('click', (event) => {
 
@@ -8,12 +51,18 @@
  	const FORM = document.forms['form'];
 
  	let name = FORM.elements['name'].value;
+	let select = FORM.elements['select'].value;
  	let number = FORM.elements['number'].value;
- 	let adress = FORM.elements['adress'].value;
- 	let comment = FORM.elements['comment'].value;
+ 	let comment = FORM.elements['comment'].value; 	
+	let adress = FORM?.elements['adress']?.value;
+
+	if (adress == undefined) {
+		adress = '---';
+	}
 
  	const data = JSON.stringify({
  		name: name,
+		select: select,
  		number: number,
  		adress: adress,
  		comment: comment
@@ -23,7 +72,7 @@
 
  	let parsedData = JSON.parse(data);
 
- 	const message = `${parsedData.name}, ${parsedData.number}, ${parsedData.adress}, ${parsedData.comment}`;
+ 	const message = `${parsedData.name},${parsedData.select}, ${parsedData.number}, ${parsedData.adress}, ${parsedData.comment}`;
 
  	sendMessage(message);
 
