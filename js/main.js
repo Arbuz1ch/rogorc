b89e30a28982ef96b8f39bd9226efca3c0de0ff9
Berty <winestnik@gmail.com>
2022-07-18 00:30:25 +0400

    Минорные фиксы

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-// const catalogButton = document.querySelector('.catalogButton');
-// const upbutton = document.querySelector('.upbutton');
-// const screens = document.querySelectorAll('.screen');
-
-// catalogButton.addEventListener('click', (event) => {
-//     event.preventDefault();
-
-//     screens[1].classList.remove('scrollup');
-//     screens[0].classList.add('up');
-// });
-
-// upbutton.addEventListener('click', (event) => {
-//     event.preventDefault();
-//     screens[0].classList.remove('up');
-//     screens[1].classList.add('scrollup');
-// });
-
 $('button[href^="#"]').on('click', function() {
     let href = $(this).attr('href');
 
