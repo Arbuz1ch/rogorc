bd7a158b4d01858a24fe49597e2cceab5bcaa229
Berty <winestnik@gmail.com>
2022-07-19 16:41:21 +0400

    Фиксы и улучшения модального окна

--- js/socials.js
+++ js/socials.js
@@ -1,25 +1,30 @@
 // Get the modal
-var modal = document.getElementById("socialsModal");
+let modal = document.getElementById("socialsModal");
+
+let arrow = document.querySelector('.socialsArrow');
 
 // Get the button that opens the modal
-var btn = document.getElementById("socBtn");
+let btn = document.getElementById("socBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
+  arrow.classList.add("activeArrow");
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  arrow.classList.remove("activeArrow");
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    arrow.classList.remove("activeArrow");
   }
 }
