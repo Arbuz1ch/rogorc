2bda9ad6399de113c63d3fd62cee4aecd266c728
Arbuz1ch <109146015+Arbuz1ch@users.noreply.github.com>
2022-07-26 23:57:09 +0400

    Добавлены карточки в корзину

--- static/js/shoppingCard.js
+++ static/js/shoppingCard.js
@@ -1,37 +1,33 @@
-// Get the modal
 let shoppingCard = document.getElementById("shoppingCard");
 
 let modalCloseArea = document.getElementById("modalCloseArea")
 
-// Get the button that opens the modal
 let basket = document.getElementById("basket");
 
 let continueShopping = document.getElementById("continueShopping");
 
-// Get the <span> element that closes the modal
-let back = document.getElementsByClassName("back")[0];
+// let back = document.getElementsByClassName("back")[0];
+
+let modalTile = document.getElementById("modalTile");
 
 let body1 = document.getElementById("body");
 
-// When the user clicks on the button, open the modal
 basket.onclick = function() {
   shoppingCard.style.display = "flex";
-  // shoppingCard.classList.add("activeShoppingCard");
+  modalTile.classList.add("activeModalTile");
   body1.style.overflow = 'hidden';
 }
 
 continueShopping.onclick = function() {
   shoppingCard.style.display = "none";
-  // shoppingCard.classList.remove("activeShoppingCard");
+  modalTile.classList.remove("activeModalTile");
   body1.style = 'none';
 }
 
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modalCloseArea) {
     shoppingCard.style.display = "none";
-    // shoppingCard.classList.remove("activeShoppingCard");
+    modalTile.classList.remove("activeModalTile");
     body1.style = 'none';
   }
 }
